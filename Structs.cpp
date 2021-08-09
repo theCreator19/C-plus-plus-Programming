59e7c02f26c44ec6ee870b13c558c9d465bb1e46
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-08-09 22:47:21 +0530

    Create Structs.cpp

---
+++ Structs.cpp
@@ -0,0 +1,29 @@
+// Structs
+
+// HackerRank Problem Solved using C++ Language.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+struct Student{
+    int age;
+    char first_name[1000];
+    char last_name[1000];
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
